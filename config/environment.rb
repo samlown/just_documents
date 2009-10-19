64c0a31c61b7f987670b6f4a1f4582076ba45e9f
Sam Lown iMac <me@samlown.com>
2009-10-19 15:54:13 +0200

    Reverting to normal gem requirements

--- config/environment.rb
+++ config/environment.rb
@@ -21,7 +21,8 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "haml"
   config.gem "RedCloth"
-  config.gem 'mislav-will_paginate', :lib => 'will_paginate', :source => 'http://gems.github.com'
+  config.gem 'will_paginate', :source => 'http://gemcutter.org/'
+  config.gem 'ruby-openid'
 
 
   # Only load the plugins named here, in the order given (default is alphabetical).
